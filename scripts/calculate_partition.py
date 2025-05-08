99725e2f11ec76d3fd8296d991ae64befceb716e
pierfrancescomartinello <pierfrancesco.martinello@outlook.it>
2025-05-08 11:09:26 +0200

    Added Comments

--- scripts/calculate_partition.py
+++ scripts/calculate_partition.py
@@ -3,18 +3,20 @@ from collections import Counter
 
 def calculate_ds(SA:list[list[str]], Child:list[list[int]], F:dict[str, int], n_0:int, n_1:int, k:int) -> tuple[list[int], list[int]]:
     '''
-    Calculates the necessary structures to compute the Jaccard distance
+    Calculates the necessary structures to compute the distance described in Dolce et Al.,2024
    
     Args:
-        SA (tuple of lists of string): The arrays SA0 and SA1, representing the labels of the two XBWTs
-        Child (tuple of lists of int): The arrays Child0 and Child1, representing the number of children each node in the two XBWTs has
-        F (dictionary of str and int): The dictionary that gives the indexes on where in the fictitious merged XBWT the prefixes that start with a particular character must be placed
+        SA (tuple of lists of string):  The arrays SA0 and SA1, representing the labels of the two XBWTs
+        Child (tuple of lists of int):  The arrays Child0 and Child1, representing the number of children each node in the two XBWTs has
+        F (dictionary of str and int):  The dictionary that gives the indexes on where in the fictitious merged XBWT the prefixes 
+                                        that start with a particular character must be placed
         n_0 (int): The size of SA0
         n_1 (int): The size of SA1
     
     Returns:
         Flag (list of int): The array showing the contribution of each one of the two XBWTs
-        LCP  (list of int): The array representing the longest common prefix between the element at index i and the one at index i-1 (LCP[0] is 0 per convention as LCP('',\\epsilon) = 0)
+        LCP  (list of int): The array representing the longest common prefix between the element at index i and the one at index i-1
+                            (LCP[0] is 0 per convention since LCP('',\\epsilon) = 0)
     '''
     
     n:int = n_0+n_1     
@@ -75,38 +77,94 @@ def calculate_ds(SA:list[list[str]], Child:list[list[int]], F:dict[str, int], n_
     
     return Flagh, [i if i != -1 else k for i in LCP]
 
+
 def calculate_partition_contributions(Flag:list[int], LCP:list[int], SA:list[list[str]], alphabet:set[str]) -> list[float]:
-    Flag_part:list[list[int]] = []
-    kn:list[int] = [0,0]
-    contributions:list[float] = []
+    '''
+    Calculates how much each set of the partition contributes to the final pseudometric.
+    The contribution (calculated following Dolce et Al, 2024) is calculated starting from the Jaccard distance
+    on the contributions (multiset of characters) from the two trees
+    
+    
+    Args:
+        Flag (list of int): The array showing the contribution of each one of the two XBWTs
+        LCP  (list of int): The array representing the longest common prefix between the element at index i and the one at index i-1
+                            (LCP[0] is 0 per convention since LCP('',\\epsilon) = 0)
+        SA (tuple of lists of string):  The arrays SA0 and SA1, representing the labels of the two XBWTs
+        alphabet (set of string): The union of the alphabets of the two XBWTs
+        
+    Returns:
+        contributions (list of float): The contribution given by each set in the partition
+     '''
+    
+    
+    Flag_part:list[list[int]] = []      # Here the partition of the Flag array will be stored
+    kn:list[int] = [0,0]                # Two variables to keep track of the current reading index for the two trees
+    contributions:list[float] = []      # Here the contributions related to each set in the partition will be stored
     
     
     # TODO: Consider substitution with https://pypi.org/project/multiset/
     def multiset_union(a:Counter, b:Counter)-> list:
+        '''
+            A method to calculate the union of two multisets.
+            The union of two multisets is defined as a multiset on which an element
+            will appear with multiplicity equal to the maximum between the multiplicity of the element in the original input
+            
+            Args:
+                a (Counter object): This represents the first multiset
+                b (Counter object): This represents the second multiset
+                
+            Returns:
+                union (list): This will be the union of the two multisets
+        '''
         return [i for i in set(a.keys()).union(set(b.keys())) for _ in range(max(a.get(i,0), b.get(i,0)))]
     
     def multiset_intersection(a:Counter, b:Counter) ->list:
+        '''
+            A method to calculate the intersection of two multisets.
+            The union of two multisets is defined as a multiset on which an element
+            will appear with multiplicity equal to the maximum between the multiplicity of the element in the original input
+            
+            Args:
+                a (Counter object): This represents the first multiset
+                b (Counter object): This represents the second multiset
+                
+            Returns:
+                union (list): This will be the intersection of the two multisets
+        '''
         return [i for i in set(a.keys()).union(set(b.keys())) for _ in range(min(a.get(i,0), b.get(i,0)))]
     
+    
     def dict_reset(d:dict)->dict:
+        '''
+            A method to reset a dictionary
+            
+            Args:
+                d (dict): The dictionary set to be reset
+            
+            Returns:
+                d (dict): The same dictionary where the values of each key is set to zero
+        '''
         return {c:0 for c in d.keys()}
             
-    
+    # This is where all the bounds of each set in the partition is stored
     index_list:list = [i for i,value in enumerate(LCP) if i == 0 or value < LCP[i-1]] + [len(LCP)]
 
+    #Splitting the orginal Flag array into a list of subarrays
     for i in range(len(index_list)-1):
         # is LCP useful? I don't think so
         # LCP_part.append(LCP[index_list[i]:index_list[i+1]])
         Flag_part.append(Flag[index_list[i]:index_list[i+1]])
 
+    # Navigating through each partition
     for i, partition in enumerate(Flag_part):
+        #This is where the elements with their multiplicity from each XBWT will be stored
         C:list[Counter] = [Counter(), Counter()]
         for b in partition:
-            
             c:str =  SA[b][kn[b]]
             C[b][c] = C[b].get(c,0) + 1
             kn[b]+=1
-            
+        
+        #Calculating the union and intersection of the multiset and using them to calculate the contribution
         uni:list[int] = multiset_union(*C)
         inter:list[int] = multiset_intersection(*C)
         contributions.append((len(uni) - len(inter))/len(uni))    
@@ -115,5 +173,18 @@ def calculate_partition_contributions(Flag:list[int], LCP:list[int], SA:list[lis
 
 
 def calculate_distance(contributions:list[float]) -> float:
+    '''
+        A simple wrapper of the sum function.
+        Useful should the definition be modified in future
+        
+        Args:
+            contributions (list of float): The contributions from each subset
+            
+        Returns:
+            distance (float): The sum of the contributions
+    '''
     return sum(contributions)
     
+
+
+
